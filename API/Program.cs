77ca6f19f70a064ca00a6b732d30e3ed8be1b1ba
Bolba-Mateescu Andrei-Ioan <34941652+andreibolba@users.noreply.github.com>
2023-02-06 17:06:03 +0200

    Inital commit

---
+++ API/Program.cs
@@ -0,0 +1,39 @@
+using System.Text.Json.Serialization;
+using API.FluentMigration;
+using API.Models;
+using Microsoft.EntityFrameworkCore;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+
+        builder.Services.AddControllers().AddJsonOptions(x =>
+                x.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles);
+        builder.Services.AddEndpointsApiExplorer();
+        builder.Services.AddSwaggerGen();
+        builder.Services.AddDbContext<SocialAppContext>(options =>
+            options.UseSqlServer(builder.Configuration.GetConnectionString("dbconn")));
+
+
+        var app = builder.Build();
+
+        Database.RunMigrations();
+
+        // Configure the HTTP request pipeline.
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI();
+        }
+
+        app.UseHttpsRedirection();
+
+        app.UseAuthorization();
+
+        app.MapControllers();
+
+        app.Run();
+    }
+}

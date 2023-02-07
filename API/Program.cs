99600290915bf8250f6b81e60b416090b99ce25c
Bolba-Mateescu Andrei-Ioan <34941652+andreibolba@users.noreply.github.com>
2023-02-08 00:08:58 +0200

    Make first request in angular

--- API/Program.cs
+++ API/Program.cs
@@ -15,23 +15,25 @@ internal class Program
         builder.Services.AddSwaggerGen();
         builder.Services.AddDbContext<SocialAppContext>(options =>
             options.UseSqlServer(builder.Configuration.GetConnectionString("dbconn")));
-
+        builder.Services.AddCors();
 
         var app = builder.Build();
 
         Database.RunMigrations();
 
-        // Configure the HTTP request pipeline.
         if (app.Environment.IsDevelopment())
         {
             app.UseSwagger();
             app.UseSwaggerUI();
         }
 
-        app.UseHttpsRedirection();
 
         app.UseAuthorization();
+        //to change to work with http://localhost/4200
+        app.UseCors(b => b.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod());
 
+        app.UseHttpsRedirection();
+        
         app.MapControllers();
 
         app.Run();

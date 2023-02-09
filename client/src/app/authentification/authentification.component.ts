52b3fdc8c92917f7d4ae48ed3e4c18f5442967a4
Bolba-Mateescu Andrei-Ioan <34941652+andreibolba@users.noreply.github.com>
2023-02-09 21:21:59 +0200

    Add register template

--- client/src/app/authentification/authentification.component.ts
+++ client/src/app/authentification/authentification.component.ts
@@ -1,11 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-authentification',
   templateUrl: './authentification.component.html',
-  styleUrls: ['./authentification.component.css']
+  styleUrls: ['./authentification.component.css'],
 })
-export class AuthentificationComponent{
+export class AuthentificationComponent implements OnInit, OnDestroy {
+  isLoginMode = true;
 
+  constructor() {}
+
+  ngOnInit(): void {
+    this.isLoginMode = true;
+  }
+  ngOnDestroy(): void {
+    this.isLoginMode = true;
+  }
+
+  onSwitchMode(form: NgForm) {
+    this.isLoginMode = !this.isLoginMode;
+    form.reset();
+  }
+
+  onSubmit(form: NgForm) {
+    form.reset();
+  }
 }
-

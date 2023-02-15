420792c231108dce70feeecd5025dbd08129210a
Bolba-Mateescu Andrei-Ioan <34941652+andreibolba@users.noreply.github.com>
2023-02-15 13:46:39 +0200

    Finish meeting tab in dashboard

--- client/src/app/intern/intern.dashboard/intern.dashboard.component.ts
+++ client/src/app/intern/intern.dashboard/intern.dashboard.component.ts
@@ -9,16 +9,24 @@ import { DashboardService } from 'src/services/dashboard.service';
 })
 export class InternDashboardComponent {
   feedback=[
-    {'feedback': 'Very good job! Keep up with the good work', 'date':' 15-Feb-2023'},
+    {'feedback': 'Very good job! Keep up with the good work!', 'date':' 15-Feb-2023'},
     {'feedback': 'Very good job! Keep up with the good work!Very good job! Keep up with the good work!', 'date':' 15-Feb-2023'},
-    {'feedback': 'Very good job! Keep up with the good work', 'date':' 15-Feb-2023'},
+    {'feedback': 'Very good job!', 'date':' 15-Feb-2023'},
   ];
   hasFeedback=true;
   feedbackLink='feedback';
 
+  meetings=[
+    {'name': 'Semestrial Meeting with the team', 'date':' 16-Feb-2023 5:00 PM', 'link': 'meetings'},
+    {'name': 'Meeting with the mentor', 'date':' 17-Feb-2023 10:00 AM', 'link': 'meetings'},
+    {'name': 'Meeting with the client', 'date':' 19-Feb-2023 1:15 PM', 'link': 'meetings'},
+  ];
+  hasMeetings=true;
+  meetingsLink='meetings';
+
   constructor(private dashServ:DashboardService){}
 
-  onfeedbackclick(){
+  onLeaveDashboardClick(){
     this.dashServ.dashboardChanged.emit(false);
   }
 }

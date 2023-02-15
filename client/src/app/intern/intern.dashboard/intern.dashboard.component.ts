64271b8c7985f10733af87732621a5a458381683
Bolba-Mateescu Andrei-Ioan <34941652+andreibolba@users.noreply.github.com>
2023-02-15 18:09:56 +0200

    Finish calendar

--- client/src/app/intern/intern.dashboard/intern.dashboard.component.ts
+++ client/src/app/intern/intern.dashboard/intern.dashboard.component.ts
@@ -1,5 +1,7 @@
 import { Component, ViewChild } from '@angular/core';
 import { MatCalendar } from '@angular/material/datepicker';
+import { Day } from 'src/model/day.model';
+import { CalendarCreatorService } from 'src/services/calendar.creator.service';
 import { DashboardService } from 'src/services/dashboard.service';
 
 @Component({
@@ -24,7 +26,54 @@ export class InternDashboardComponent {
   hasMeetings=true;
   meetingsLink='meetings';
 
-  constructor(private dashServ:DashboardService){}
+  public monthDays!: Day[];
+
+  public monthNumber!: number;
+  public year!: number;
+
+  public weekDaysName:string[] = [];
+
+  constructor(public calendarCreator: CalendarCreatorService, private dashServ:DashboardService) {}
+
+  ngOnInit(): void {
+    this.setMonthDays(this.calendarCreator.getCurrentMonth());
+
+    this.weekDaysName.push("Mo");
+    this.weekDaysName.push("Tu");
+    this.weekDaysName.push("We");
+    this.weekDaysName.push("Th");
+    this.weekDaysName.push("Fr");
+    this.weekDaysName.push("Sa");
+    this.weekDaysName.push("Su");
+  }
+
+  onNextMonth(): void {
+    this.monthNumber++;
+
+    if (this.monthNumber == 13) {
+      this.monthNumber = 1;
+      this.year++;
+    }
+
+    this.setMonthDays(this.calendarCreator.getMonth(this.monthNumber, this.year));
+  }
+
+  onPreviousMonth() : void{
+    this.monthNumber--;
+
+    if (this.monthNumber < 1) {
+      this.monthNumber = 12;
+      this.year--;
+    }
+
+    this.setMonthDays(this.calendarCreator.getMonth(this.monthNumber, this.year));
+  }
+
+  private setMonthDays(days: Day[]): void {
+    this.monthDays = days;
+    this.monthNumber = this.monthDays[0].monthIndex;
+    this.year = this.monthDays[0].year;
+  }
 
   onLeaveDashboardClick(){
     this.dashServ.dashboardChanged.emit(false);

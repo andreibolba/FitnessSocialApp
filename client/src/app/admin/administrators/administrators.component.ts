15e1e0caa19b2220a90350d9d4fb531219e40ddb
Bolba-Mateescu Andrei-Ioan <34941652+andreibolba@users.noreply.github.com>
2023-02-17 11:40:59 +0200

    Add subscription

--- client/src/app/admin/administrators/administrators.component.ts
+++ client/src/app/admin/administrators/administrators.component.ts
@@ -1,10 +1,4 @@
-import {
-  Component,
-  AfterViewInit,
-  OnDestroy,
-  OnInit,
-  ViewChild,
-} from '@angular/core';
+import { Component, OnDestroy, OnInit, ViewChild } from '@angular/core';
 import { DataStorageService } from 'src/services/data-storage.service';
 import { Subscription } from 'rxjs';
 import { Person } from 'src/model/person.model';
@@ -17,24 +11,40 @@ import { MatTableDataSource } from '@angular/material/table';
   templateUrl: './administrators.component.html',
   styleUrls: ['./administrators.component.css'],
 })
-export class AdministratorsComponent {
-  displayedColumns: string[] = ['firstName', 'lastName', 'email',  'username', 'birthDate'];
+export class AdministratorsComponent implements OnInit, OnDestroy {
+  displayedColumns: string[] = [
+    'firstName',
+    'lastName',
+    'email',
+    'username',
+    'birthDate',
+  ];
   dataSource!: MatTableDataSource<Person>;
   people!: Person[];
+  dataPeopleSub!: Subscription;
   @ViewChild(MatPaginator) paginator!: MatPaginator;
 
-  constructor(private service: DataStorageService) {
+  constructor(private service: DataStorageService) {}
+
+  ngOnInit(): void {
     const personString = localStorage.getItem('person');
     if (!personString) {
       return;
     } else {
       const person: LoggedPerson = JSON.parse(personString);
-    this.service.getPeople(person.token).subscribe((data) => {
-      this.people = data;
-      this.dataSource = new MatTableDataSource(this.people.filter(p=>p.status=='Admin'));
-      this.dataSource.paginator = this.paginator;
-    });
+      this.dataPeopleSub = this.service
+        .getPeople(person.token)
+        .subscribe((data) => {
+          this.people = data;
+          this.dataSource = new MatTableDataSource(
+            this.people.filter((p) => p.status == 'Admin')
+          );
+          this.dataSource.paginator = this.paginator;
+        });
+    }
   }
+
+  ngOnDestroy(): void {
+    if (this.dataPeopleSub) this.dataPeopleSub.unsubscribe();
   }
 }
-

e5815fcecd2bc8335dfc87e2ac846d3461129233
Bolba-Mateescu Andrei-Ioan <34941652+andreibolba@users.noreply.github.com>
2023-02-17 11:26:05 +0200

    Have a functional table

--- client/src/app/admin/administrators/administrators.component.ts
+++ client/src/app/admin/administrators/administrators.component.ts
@@ -12,51 +12,34 @@ import { LoggedPerson } from 'src/model/loggedperson.model';
 import { MatPaginator } from '@angular/material/paginator';
 import { MatTableDataSource } from '@angular/material/table';
 
+export interface UserData {
+  id: string;
+  userId: string;
+  title: any;
+  body: any;
+}
+
 
 @Component({
   selector: 'app-administrators',
   templateUrl: './administrators.component.html',
   styleUrls: ['./administrators.component.css'],
 })
-export class AdministratorsComponent implements OnInit,OnDestroy {
-  isLoading = false;
-  dataPeopleSub!: Subscription;
-  peopleTest!:Person[];
-  displayedColumns: string[] = ['firstName', 'lastName', 'email', 'username','birthDate'];
-  dataSource = new MatTableDataSource<Person>();
-
+export class AdministratorsComponent {
+  displayedColumns: string[] = ['id', 'userId', 'title'];
+  dataSource!: MatTableDataSource<UserData>;
+  posts: any;
   @ViewChild(MatPaginator) paginator!: MatPaginator;
 
-  constructor(private dataService: DataStorageService) {
-  }
-  
-  ngOnInit(): void {
-    this.isLoading=true;
-    this.setCurrentUser();
-  }
-  ngOnDestroy(): void {
-    if(this.dataPeopleSub)this.dataPeopleSub.unsubscribe();
-  }
+  constructor(private service: DataStorageService) {
+    this.service.getData().subscribe((data) => {
+      console.log(data);
+      this.posts = data;
+      this.dataSource = new MatTableDataSource(this.posts);
 
-  setCurrentUser() {
-    const personString = localStorage.getItem('person');
-    if (!personString) {
-      return;
-    } else {
-      const person: LoggedPerson = JSON.parse(personString);
-      this.dataPeopleSub = this.dataService.getPeople(person.token).subscribe(
-        (res) => {
-          this.peopleTest = res;
-        },
-        (error) => {
-          console.log(error.error);
-        },()=>{
-          this.isLoading=false;
-          this.dataSource = new MatTableDataSource<Person>(this.peopleTest);
-          this.dataSource.paginator = this.paginator;
-          console.log(this.peopleTest);
-        }
-      );
-    }
+      this.dataSource.paginator = this.paginator;
+    });
+
   }
 }
+

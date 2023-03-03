e1e709738f25e3793ad876d145423e06b193fcdb
Bolba-Mateescu Andrei-Ioan <34941652+andreibolba@users.noreply.github.com>
2023-03-03 14:18:58 +0200

    IH-23 Add repository for password link

---
+++ API/Data/PasswordLinkRepository.cs
@@ -0,0 +1,46 @@
+﻿using API.Interfaces.Repository;
+using API.Models;
+
+namespace API.Data
+{
+    public class PasswordLinkRepository : IPasswordLinkRepository
+    {
+        private readonly InternShipAppSystemContext _context;
+
+        public PasswordLinkRepository(InternShipAppSystemContext context)
+        {
+            _context = context;
+        }
+
+        public void Create(PasswordkLink link)
+        {
+            _context.PasswordkLinks.Add(link);
+        }
+
+        public void Delete(PasswordkLink link)
+        {
+            link.Deleted = true;
+            this.Update(link);
+        }
+
+        public IEnumerable<PasswordkLink> GetAll()
+        {
+            return _context.PasswordkLinks.ToList().Where(p=>p.Deleted==false);
+        }
+
+        public PasswordkLink GetById(int id)
+        {
+            return this.GetAll().SingleOrDefault(p => p.PasswordLinkId == id);
+        }
+
+        public bool SaveAll()
+        {
+            return _context.SaveChanges() > 0;
+        }
+
+        public void Update(PasswordkLink link)
+        {
+            _context.PasswordkLinks.Update(link);
+        }
+    }
+}
---
+++ API/Interfaces/Repository/IPasswordLinkRepository.cs
@@ -0,0 +1,14 @@
+﻿using API.Models;
+
+namespace API.Interfaces.Repository
+{
+    public interface IPasswordLinkRepository
+    {
+        void Create(PasswordkLink link);
+        IEnumerable<PasswordkLink> GetAll();
+        PasswordkLink GetById(int id);
+        void Update(PasswordkLink link);
+        void Delete(PasswordkLink link);
+        bool SaveAll();
+    }
+}

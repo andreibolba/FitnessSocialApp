1e4bffd6352b6e16a44fbd0d4b5d2366b47d30f7
Bolba-Mateescu Andrei-Ioan <34941652+andreibolba@users.noreply.github.com>
2023-03-09 15:14:31 +0200

    IH-10 Necessary files

--- API/Program.cs
+++ API/Program.cs
@@ -1,12 +1,6 @@
-using System.Text;
 using System.Text.Json.Serialization;
 using API.Extensions;
 using API.FluentMigration;
-//using API.Interfaces;
-//using API.Services;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.IdentityModel.Tokens;
 
 internal class Program
 {
@@ -21,7 +15,7 @@ internal class Program
 
         var app = builder.Build();
 
-        Database.RunMigrations();
+        //Database.RunMigrations();
 
         app.UseCors(b => b.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod());
 

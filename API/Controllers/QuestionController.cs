424af3fe362c2252ff00b8acaa2059460e43626d
Bolba-Mateescu Andrei-Ioan <34941652+andreibolba@users.noreply.github.com>
2023-03-09 15:08:42 +0200

    IH-13 Add repository for question

---
+++ API/Controllers/QuestionController.cs
@@ -0,0 +1,57 @@
+﻿using API.Dtos;
+using API.Interfaces.Repository;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    public class QuestionController : BaseAPIController
+    {
+        private readonly IQuestionRepository _question;
+
+        public QuestionController(IQuestionRepository question)
+        {
+            _question = question;
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<QuestionDto>> GetAllQuestions()
+        {
+            return Ok(_question.GetAllQuestions());
+        }
+
+        [HttpGet("{questionId:int}")]
+        public ActionResult<IEnumerable<QuestionDto>> GetQuetionById(int questionId)
+        {
+            return Ok(_question.GetQuestionById(questionId));
+        }
+
+
+        [HttpPost("add")]
+        public ActionResult AddQuestion([FromBody] QuestionDto question)
+        {
+            _question.Create(question);
+            return _question.SaveAll() ? Ok() : BadRequest("Internal Server Error");
+        }
+
+        [HttpPost("delete/{questionId:int}")]
+        public ActionResult DeleteQuestion(int questionId)
+        {
+            _question.Delete(questionId);
+            return _question.SaveAll() ? Ok() : BadRequest("Internal Server Error");
+        }
+
+        [HttpPost("stop/{questionId:int}")]
+        public ActionResult StopEditQuestion(int questionId)
+        {
+            _question.StopEdit(questionId);
+            return _question.SaveAll() ? Ok() : BadRequest("Internal Server Error");
+        }
+
+        [HttpPost("update")]
+        public ActionResult UpdateQuestion([FromBody] QuestionDto question)
+        {
+            _question.Update(question);
+            return _question.SaveAll() ? Ok() : BadRequest("Internal Server Error"); ;
+        }
+    }
+}
---
+++ API/Interfaces/Repository/IQuestionRepository.cs
@@ -0,0 +1,15 @@
+﻿using API.Dtos;
+
+namespace API.Interfaces.Repository
+{
+    public interface IQuestionRepository
+    {
+        void Create(QuestionDto questionDto);
+        IEnumerable<QuestionDto> GetAllQuestions();
+        QuestionDto GetQuestionById(int id);
+        void Update(QuestionDto questionDto);
+        void Delete(int questionid);
+        void StopEdit(int id);
+        bool SaveAll();
+    }
+}

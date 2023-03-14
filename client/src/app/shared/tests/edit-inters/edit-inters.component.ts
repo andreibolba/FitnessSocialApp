677244e6aac3cf4718689f24c6ef6a2e906e196e
Bolba-Mateescu Andrei-Ioan <34941652+andreibolba@users.noreply.github.com>
2023-03-14 10:48:31 +0200

    IH-20 Add update intern component

---
+++ client/src/app/shared/tests/edit-inters/edit-inters.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-edit-inters',
+  templateUrl: './edit-inters.component.html',
+  styleUrls: ['./edit-inters.component.css']
+})
+export class EditIntersComponent {
+
+}

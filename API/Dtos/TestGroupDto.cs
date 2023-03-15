3f6ba10453d5cf8fff34f9e6f6bf20ada1283d02
Bolba-Mateescu Andrei-Ioan <34941652+andreibolba@users.noreply.github.com>
2023-03-15 13:06:11 +0200

    IH-12 Add Dto for interns and groups

---
+++ API/Dtos/TestGroupDto.cs
@@ -0,0 +1,10 @@
+﻿namespace API.Dtos
+{
+    public class TestGroupDto
+    {
+        public int TestGroupId { get; set; }
+        public int GroupId { get; set; }
+        public string GroupName { get; set; }
+        public bool IsChecked { get; set; }
+    }
+}
---
+++ API/Dtos/TestInternDto.cs
@@ -0,0 +1,12 @@
+﻿namespace API.Dtos
+{
+    public class TestInternDto
+    {
+        public int TestInternId { get; set; }
+        public int InternId { get; set; }
+        public int TestId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public bool IsChecked { get; set; }
+    }
+}

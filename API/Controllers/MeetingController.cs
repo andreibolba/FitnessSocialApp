a8db0f594378ae100c89eddb890d9f544c3e5e89
Bolba-Mateescu Andrei-Ioan <34941652+andreibolba@users.noreply.github.com>
2023-03-16 13:00:50 +0200

    IH-29 Implement controller and repo for meeting

--- API/Controllers/MeetingController.cs
+++ API/Controllers/MeetingController.cs
@@ -1,4 +1,6 @@
-﻿using API.Interfaces.Repository;
+﻿using API.Dtos;
+using API.Interfaces.Repository;
+using Microsoft.AspNetCore.Mvc;
 
 namespace API.Controllers
 {
@@ -11,6 +13,54 @@ namespace API.Controllers
             _repository = repository;
         }
 
+        [HttpGet]
+        public ActionResult GetAllMeetings()
+        {
+            return Ok(_repository.GetAll());
+        }
 
+        [HttpGet("/{meetId:int}")]
+        public ActionResult GetOneMeeting(int meetId)
+        {
+            return Ok(_repository.GetMeetingById(meetId));
+        }
+
+        [HttpGet("/{id:int}/{status}")]
+        public ActionResult GetMeetingForPerson(int id, string status)
+        {
+            switch(status)
+            {
+                case "inter":
+                    return Ok(_repository.GetAllByInternId(id));
+                case "trainer":
+                    return Ok(_repository.GetAllByTrainerId(id));
+                case "group":
+                    return Ok(_repository.GetAllByGroupId(id));
+                default:
+                    return BadRequest("Invalid option");
+
+            }
+        }
+
+        [HttpPost("/add")]
+        public ActionResult Create([FromBody] MeetingDto meeting)
+        {
+            var meet = _repository.Create(meeting);
+            return _repository.SaveAll() ? Ok(meet) : BadRequest("Internal Server Error");
+        }
+
+        [HttpPost("/update")]
+        public ActionResult Update([FromBody] MeetingDto meeting)
+        {
+            _repository.Update(meeting);
+            return _repository.SaveAll() ? Ok() : BadRequest("Internal Server Error");
+        }
+
+        [HttpPost("/delete/{meetId:int}")]
+        public ActionResult Delete(int meetId)
+        {
+            _repository.Delete(meetId);
+            return _repository.SaveAll() ? Ok() : BadRequest("Internal Server Error");
+        }
     }
 }

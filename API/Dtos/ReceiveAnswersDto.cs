d75fd240bc1e58b4c36f94bcace7c1e9217ba489
Bolba-Mateescu Andrei-Ioan <34941652+andreibolba@users.noreply.github.com>
2023-03-19 06:15:00 +0200

    IH-17 Add dto for answers

---
+++ API/Dtos/ReceiveAnswersDto.cs
@@ -0,0 +1,13 @@
+﻿namespace API.Dtos
+{
+    public class Answer
+    {
+        public int QuestionId { get; set; }
+        public string InternOption { get; set; }
+    }
+    public class ReceiveAnswersDto
+    {
+        public int InternId { get; set; }
+        public List<Answer> Answers { get; set;}
+    }
+}

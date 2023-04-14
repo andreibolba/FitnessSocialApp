8c0d23a25b2877e2c8c1d76b2a9b37752e12dce2
Bolba-Mateescu Andrei-Ioan <34941652+andreibolba@users.noreply.github.com>
2023-04-14 08:58:07 +0300

    IH-31 Add post picture table

---
+++ API/FluentMigration/Migrations/PostPicturesTable.cs
@@ -0,0 +1,28 @@
+﻿using FluentMigrator;
+
+namespace API.FluentMigration.Migrations
+{
+    public class PostPicturesTable : Migration
+    {
+        public const string tableName = "PostPictures";
+        public const string fkName = "PostPicturesPostFK";
+        public override void Down()
+        {
+            Delete.Table(tableName);
+            Delete.ForeignKey(fkName);
+        }
+
+        public override void Up()
+        {
+            Create.Table(tableName)
+                .WithColumn("PostPicturesId").AsInt32().PrimaryKey().NotNullable().Identity()
+                .WithColumn("PostId").AsInt32().NotNullable()
+                .WithColumn("Picture").AsBinary().Nullable()
+                .WithColumn("Deleted").AsBoolean().NotNullable();
+
+            Create.ForeignKey(fkName)
+               .FromTable(tableName).ForeignColumn("PostId")
+               .ToTable(PostTable.tableName).PrimaryColumn("PostId");
+        }
+    }
+}

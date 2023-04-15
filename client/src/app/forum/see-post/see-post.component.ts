a6813183bed062eff9c9514cdaf0997c87a948ee
Bolba-Mateescu Andrei-Ioan <34941652+andreibolba@users.noreply.github.com>
2023-04-15 13:30:44 +0300

    IH-40 Finish see the post

--- client/src/app/forum/see-post/see-post.component.ts
+++ client/src/app/forum/see-post/see-post.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-see-post',
   templateUrl: './see-post.component.html',
   styleUrls: ['./see-post.component.css']
 })
-export class SeePostComponent {
+export class SeePostComponent implements OnInit, OnDestroy{
+  date: Date=new Date();
+
+  constructor(){
+
+  }
+  ngOnInit(): void {
+    throw new Error('Method not implemented.');
+  }
+  ngOnDestroy(): void {
+    throw new Error('Method not implemented.');
+  }
+
 
 }

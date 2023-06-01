b79ee591203368a64572bcb9b0978503005ebd69
Bolba-Mateescu Andrei-Ioan <andrei_bolba@yahoo.com>
2023-06-01 11:05:50 +0300

    IH-76 Implement Controller

---
+++ API/Controllers/NoteController.cs
@@ -0,0 +1,46 @@
+﻿using API.Dtos;
+using API.Interfaces.Repository;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [Authorize]
+    public class NoteController:BaseAPIController
+    {
+        private readonly INoteRepository _noteRepository;
+
+        public NoteController(INoteRepository noteRepository)
+        {
+            _noteRepository = noteRepository;
+        }
+
+        [HttpGet]
+        public ActionResult GetAllNotes()
+        {
+            return Ok(_noteRepository.GetAllNotes());
+        }
+
+        [HttpGet("{noteId}")]
+        public ActionResult GetNoteById(int noteId)
+        {
+            return Ok(_noteRepository.GetNoteById(noteId));
+        }
+
+        [HttpPost("add")]
+        public ActionResult AddNote([FromBody] NoteDto note)
+        {
+            var res = _noteRepository.CreateNote(note);
+
+            return res != null? Ok(res) : BadRequest();
+        }
+
+        [HttpPost("edit")]
+        public ActionResult EditNote([FromBody] NoteDto note)
+        {
+            var res = _noteRepository.UpdateNote(note);
+
+            return res != null ? Ok(res) : BadRequest();
+        }
+    }
+}

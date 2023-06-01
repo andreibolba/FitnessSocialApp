c47b83ae86e2bef85d7d0913300f4bef1204c29a
Bolba-Mateescu Andrei-Ioan <andrei_bolba@yahoo.com>
2023-06-01 14:44:00 +0300

    IH-82 Create repositoties

---
+++ API/Interfaces/Repository/IChallangeRepository.cs
@@ -0,0 +1,15 @@
+﻿using API.Dtos;
+
+namespace API.Interfaces.Repository
+{
+    public interface IChallangeRepository
+    {
+        public ChallangeDto CreateChallange(ChallangeDto challange);
+        public ChallangeDto UpdateChallange(ChallangeDto challange);
+        public void DeleteChallange(int id);
+        public ChallangeDto GetChallangeById(int id);
+        public IEnumerable<ChallangeDto> GetAllChallanges();
+        public IEnumerable<ChallangeDto> GetAllChallangesForSpecificDay(DateTime time);
+        public bool SaveAll();
+    }
+}
---
+++ API/Interfaces/Repository/IChallangeSolutionRpository.cs
@@ -0,0 +1,18 @@
+﻿using API.Dtos;
+
+namespace API.Interfaces.Repository
+{
+    public interface IChallangeSolutionRpository
+    {
+        public ChallangeSolutionDto CreateSolution(ChallangeSolutionDto solution);
+        public ChallangeSolutionDto UpdateSolution(ChallangeSolutionDto solution);
+        public void DeleteSolution(int id);
+        public ChallangeSolutionDto GetSolutionById(int id);
+        public IEnumerable<ChallangeSolutionDto> GetAllSolutions();
+        public IEnumerable<ChallangeSolutionDto> GetAllSolutionsForIntern(int internId);
+        public IEnumerable<ChallangeSolutionDto> GetAllSolutionsForChallange(int challangeId);
+        public IEnumerable<ChallangeSolutionDto> GetAllSolutionsForSpecificDay(DateTime time);
+        public bool SaveAll();
+
+    }
+}

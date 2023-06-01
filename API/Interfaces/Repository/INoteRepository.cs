7ea65ce2c8da2401aae2616692a78dc7ef117a6d
Bolba-Mateescu Andrei-Ioan <andrei_bolba@yahoo.com>
2023-06-01 10:54:05 +0300

    IH-70 Create repository

---
+++ API/Interfaces/Repository/INoteRepository.cs
@@ -0,0 +1,14 @@
+﻿using API.Dtos;
+
+namespace API.Interfaces.Repository
+{
+    public interface INoteRepository
+    {
+        public NoteDto CreateNote(NoteDto note);
+        public NoteDto UpdateNote(NoteDto note);
+        public NoteDto DeleteNote(int noteId);
+        public IEnumerable<NoteDto> GetAll();
+        public NoteDto GetNote(int noteId);
+        public bool SaveAll();
+    }
+}
